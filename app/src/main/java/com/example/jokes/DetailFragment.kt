aab5deeeae92804eb34a84ba1ab85a6f7b5a59b4
jade-r-k <jade.r.s.kelly@gmail.com>
2021-11-22 13:00:52 +0000

    Detail Fragment

---
+++ app/src/main/java/com/example/jokes/DetailFragment.kt
@@ -0,0 +1,27 @@
+package com.example.jokes
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// Detail Fragment is used to display joke in better detail
+class DetailFragment : Fragment() {
+
+    private lateinit var viewModel: DetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.detail_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/example/jokes/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.jokes
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

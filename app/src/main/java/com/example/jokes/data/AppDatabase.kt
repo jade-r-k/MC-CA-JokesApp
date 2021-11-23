df2f422b8f09661f889779775236072ce9adc744
jade-r-k <jade.r.s.kelly@gmail.com>
2021-11-23 21:53:43 +0000

    Database with Room

---
+++ app/src/main/java/com/example/jokes/data/AppDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.jokes.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [JokeEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun jokeDao(): JokeDao?
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase? {
+            if (INSTANCE == null) {
+                synchronized(AppDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "jokesapp.db"
+                    ).build()
+                }
+            }
+
+            return INSTANCE
+        }
+    }
+}

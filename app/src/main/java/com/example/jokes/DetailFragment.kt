1061230bf91453b0991ddf36e59bb1ff64e0307a
jade-r-k <jade.r.s.kelly@gmail.com>
2021-11-23 20:07:58 +0000

    Navigate to previous fragment

--- app/src/main/java/com/example/jokes/DetailFragment.kt
+++ app/src/main/java/com/example/jokes/DetailFragment.kt
@@ -5,8 +5,12 @@ import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.jokes.databinding.DetailFragmentBinding
 
@@ -21,9 +25,40 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        (activity as AppCompatActivity).supportActionBar?.let {
+            it.setHomeButtonEnabled(true)
+            it.setDisplayShowHomeEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_back)
+        }
+        setHasOptionsMenu(true)
+
         binding = DetailFragmentBinding.inflate(inflater, container, false)
         binding.jokeText.setText("You selected joke number ${args.jokeId}")
+
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true){
+                override fun handleOnBackPressed() {
+                    saveAndReturn()
+                }
+            }
+        )
+
         return binding.root
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> saveAndReturn()
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun saveAndReturn(): Boolean {
+        findNavController().navigateUp()
+
+        return true
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
